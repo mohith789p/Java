6ede79ac13e14658deeb6a457b259b3e0f6c8de2
mohith789p <mohith321p@gmail.com>
2024-07-10 12:44:22 +0530

    Square Root by user input

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class SquareRoot{
 	public static void main(String[] args){
-		double x=25,y;
+		double x,y;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the Number: ");
+		x = scanner.nextDouble();
 		y = Math.sqrt(x);
 		System.out.println("Square Root of " + x + " is " + y);
 	}
