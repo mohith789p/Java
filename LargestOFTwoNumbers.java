80a8588759ef04fb162a36eea948f4bf7253a6ef
mohith789p <mohith321p@gmail.com>
2024-07-10 12:01:12 +0530

    LARGEST OF TWO NUMBERS

---
+++ LargestOFTwoNumbers.java
@@ -0,0 +1,12 @@
+public class LargestOFTwoNumbers{
+	
+	public static void main(String[] args){
+		int a=10,b=5;
+		if (a>b)
+			System.out.println(a+" is Big");
+		else if (a==b)
+			System.out.println("a and b are " +b +" and Both are Equal.");
+		else
+			System.out.println(b+" is Big");
+	}
+}
