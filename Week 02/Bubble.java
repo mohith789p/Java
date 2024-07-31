cb729e869d20129666eea9c2f93eadc2d5a359f0
mohith789p <mohith321p@gmail.com>
2024-07-31 22:03:00 +0530

    Add files via upload

---
+++ Week 02/Bubble.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Sort {
+    private int[] a;
+
+    public void getValues(int[] a) {
+                this.a = a;
+    }
+
+    void bubbleSort() {
+        int i, j, temp;
+        int n = a.length;
+        for (i = 0; i < n - 1; i++) {
+            for (j = 0; j < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+public class Bubble {
+    public static void main(String[] args) {
+        Sort obj = new Sort();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        obj.getValues(arr);
+        obj.bubbleSort();
+
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
