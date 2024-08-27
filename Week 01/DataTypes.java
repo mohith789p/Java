670b19ae7ed1c09044e99bb1ed959460dbc8af5c
mohith789p <mohith321p@gmail.com>
2024-08-27 21:23:18 +0530

    commit

--- Week 01/DataTypes.java
+++ Week 01/DataTypes.java
@@ -1,21 +1,22 @@
-public class DataTypes{
-		 static byte data_byte;
-		 static short data_short;
-		 static int data_int;
-		 static float data_float;
-		 static double data_double;
-		 static long data_long;
-		 static boolean data_boolean;
-		 static char data_char; 
+public class DataTypes {
+	static byte data_byte;
+	static short data_short;
+	static int data_int;
+	static float data_float;
+	static double data_double;
+	static long data_long;
+	static boolean data_boolean;
+	static char data_char;
+
 	public static void main(String[] args) {
-		 
-		System.out.println("byte: "+ data_byte);
-		System.out.println("short: "+ data_short);
-		System.out.println("int: "+ data_int);
-		System.out.println("float: "+ data_float);
-		System.out.println("double: "+ data_double);
-		System.out.println("long: "+ data_long);
-		System.out.println("boolean: "+ data_boolean);
-		System.out.println("char: "+ data_char);		
+
+		System.out.println("byte: " + data_byte);
+		System.out.println("short: " + data_short);
+		System.out.println("int: " + data_int);
+		System.out.println("float: " + data_float);
+		System.out.println("double: " + data_double);
+		System.out.println("long: " + data_long);
+		System.out.println("boolean: " + data_boolean);
+		System.out.println("char: " + data_char);
 	}
 }
--- Week 01/QuadraticRoots2.java
+++ Week 01/QuadraticRoots2.java
@@ -1,33 +1,37 @@
 import java.util.Scanner;
 
 public class QuadraticRoots2 {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+        // equation is ax2 + bx = 0
+        System.out.print("Enter the coefficient a: ");
+        double a = sc.nextDouble();
 
-        // equation is in form ax2 + bx = 0
-        System.out.print("Enter coefficient a: ");
-        double a = scanner.nextDouble();
-        System.out.print("Enter coefficient b: ");
-        double b = scanner.nextDouble();
-        // if both are zeros then it has infinetly many solutions.
-        if (a == 0 && b == 0) {
-            System.out.println("The equation is 0 = 0 which has infinitely many solutions.");
-        } else if (a == 0) {
-            // When a is zero, the equation becomes bx = 0 => x = 0
-            System.out.println("The root of the equation is x = 0");
+        System.out.print("Enter the coefficient b: ");
+        double b = sc.nextDouble();
+
+        // Discriminant D = b*b - 4ac (here c = 0)
+        double D = b * b;
+
+        // Checking the nature of the roots
+        if (D > 0) {
+            // ax2 + bx = 0 => ( x = 0) or (ax + b = 0)
+            double root1 = -b / (2 * a);
+            double root2 = 0;
+            System.out.println("Roots are real and distinct.");
+            System.out.println("Root 1: " + root1);
+            System.out.println("Root 2: " + root2);
+        } else if (D == 0) {
+            // repeated root
+            double root = -b / (2 * a);
+            System.out.println("Roots are real and equal.");
+            System.out.println("Root: " + root);
         } else {
-            // ( x = 0 ) or ( ax - b = 0 )
-            double root1 = 0; // Root when x = 0
-            double root2 = -b / a; // Root when ax + b = 0
-
-            System.out.println("The roots of the equation are:");
-            System.out.println("x = " + root1);
-            System.out.println("x = " + root2);
+            // This case will not happen as D = b^2 (which is always >= 0)
+            System.out.println("Error: Discriminant should not be negative.");
         }
 
-        // Close the scanner
-        scanner.close();
+        sc.close();
     }
 }
